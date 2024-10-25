bbeac97f6cb9655b417faa62430044921f025205
Roberto Hortal Muñoz <rhortal@gmail.com>
2024-10-25 15:46:19 +0200

    Updated code - still no testing

--- run.py
+++ run.py
@@ -11,9 +11,10 @@ from urllib.parse import urlparse
 load_dotenv()
 
 # IMAP server settings
-IMAP_SERVER = 'imap.gmail.com'
+IMAP_SERVER = os.getenv('IMAP_SERVER'', 'imap.gmail.com')
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 APP_PASSWORD = os.getenv('APP_PASSWORD')
+ATTACHMENT_PATH = os.getenv('ATTACHMENT_PATH', 'attachments') # path to save attachments in the current directory.
 
 # get list of email bodies from INBOX folder
 with MailBox(IMAP_SERVER).login(EMAIL_ADDRESS, APP_PASSWORD, 'INBOX') as mailbox:
@@ -27,10 +28,12 @@ for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
 
 # Delete every third link, only the first 2 of each message are valid
 while links: [del links[index] for index in reversed(range(2, len(my_list), 3))]
+# Best replace this with something that looks for the right string on the URL
 
 for result in links:    
     resp = requests.get(result)
-    output = open(os.path.basename(result.path), 'wb')
+    filename = os.path.basename(result.path)
+    output = open(f'{ATTACHMENT_PATH}/{filename}' , 'wb')
     output.write(resp.content)
     output.close()
 
