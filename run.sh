105356def76a3517971ade66a4e691b23946e35b
Roberto Hortal <rhortal@gmail.com>
2024-10-30 10:44:12 +0100

    Update run.sh to take folders into account

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 run.py
