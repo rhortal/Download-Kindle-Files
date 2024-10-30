d0b7e67b1993f459f32d96158942d1af05aa9bea
Roberto Hortal <rhortal@gmail.com>
2024-10-30 14:24:45 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ cd $SCRIPT_DIR
 
 if [ ! -d .venv ]; then
   python3 -m venv .venv;
+  source .venv/bin/activate
+  python3 -m pip install -r requirements.txt
 fi
 
 source .venv/bin/activate
-python3 -m pip install -r requirements.txt
 python3 run.py
