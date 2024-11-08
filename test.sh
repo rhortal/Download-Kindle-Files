eaca6e57d67dcf25221f08cdc18906ed4f052832
Roberto Hortal Muñoz <rhortal@gmail.com>
2024-11-08 07:51:15 +0100

    Added test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+if [ ! -d .venv ]; then
+  python3 -m venv .venv;
+  source .venv/bin/activate
+  python3 -m pip install -r requirements.txt
+fi
+
+source .venv/bin/activate
+python3 -m unittest discover -s tests -v
